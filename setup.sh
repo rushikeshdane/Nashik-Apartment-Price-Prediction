1b219bd597bed9c0d0f496e2e67d3f63280f41e7
rushikeshdane <78649021+rushikeshdane@users.noreply.github.com>
2022-04-04 10:49:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
-mkdir -p ~/.streamlit
 echo "[theme]
-base='light'
-primaryColor='#0029f7'
-secondaryBackgroundColor='#ffc6a1'
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
 [server]
 headless = true
 port = $PORT
 enableCORS = false
 " > ~/.streamlit/config.toml
+
